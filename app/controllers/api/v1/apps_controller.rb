db2471a7540dafd79ee5ec6d13a753de2cb470c3
dharanimanne <dharani.manne@gmail.com>
2015-06-20 12:26:09 +0530

    churning out stuff

--- app/controllers/api/v1/apps_controller.rb
+++
@@ -1,6 +0,0 @@
-module Api
-    module V1
-        class AppsController < ApplicationController
-        end
-    end
-end
--- db/migrate/20150609104502_create_apps.rb
+++
@@ -1,8 +0,0 @@
-class CreateApps < ActiveRecord::Migration
-  def change
-    create_table :apps do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
