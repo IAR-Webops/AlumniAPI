e5ed5e45aada4a1780277b16b6f8f0063d6f80d4
dharanimanne <dharani.manne@gmail.com>
2015-06-25 12:51:06 +0530

    terms and conditions added, titles for pages modified and other changes implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
     match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
     match '/signout', to: 'sessions#destroy', via: [:get, :post]
+    get '/terms', to: 'static#terms'
 
     namespace :access do
     end
